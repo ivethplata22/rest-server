6ede010c0913f430a47ae753184395388413d2eb
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-04 07:55:48 -0600

    Tipos de peticiones

--- models/server.js
+++ models/server.js
@@ -18,7 +18,33 @@ class Server {
 
     routes() {
         this.app.get('/api', (req, res) => {
-            res.send('Hello World')
+            res.json({
+                msg: 'Get Api'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'Put Api'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                msg: 'Post Api'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'Delete Api'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'Patch Api'
+            });
         });
     }
 
