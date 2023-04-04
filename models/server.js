419eccb06f9098612ba57ab325be9d417bb2d1ce
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-04 14:08:59 -0600

    Conexion Base de Datos

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors');
+const { dbConnection } = require('../db/config');
 
 class Server {
     constructor() {
@@ -7,11 +8,18 @@ class Server {
         this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
 
+        // Conectar a Base de Datos
+        this.conectarDB();
+
         // Middlewares
         this.middlewares();
 
         // Rutas de mi aplicacion
         this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
 
     middlewares() {
