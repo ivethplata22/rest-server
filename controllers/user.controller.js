e4c98092ad4c6d083ede6c666f9dd1e9e1ded70a
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-04 21:22:15 -0500

    Validar correo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
     const { q, nombre, apikey } = req.query;
@@ -15,23 +16,34 @@ const usuariosGet = (req = request, res = response) => {
 
 // Crear usuario
 const usuariosPost = async (req = request, res = response) => {
+    // Validar formato del correo
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+        res.status(400).json(errors);
+
+    // Extraer datos del request
     const { nombre, correo, password, rol } = req.body;
     // Modelo de usuario
     const usuario = new Usuario({nombre, correo, password, rol});
 
     // Verificar si correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if(existeEmail)
+        res.status(400).json({msg: 'El correo ya esta registrado'});
+    
+    if(!existeEmail) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        usuario.password = bcryptjs.hashSync( password, salt );
 
-    // Encriptar la contraseña
-    const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync( password, salt );
+        // Guardar en BD
+        await usuario.save();
 
-    // Guardar en BD
-    await usuario.save();
-
-    res.status(201).json({
-        msg: 'Usuario Creado Con Exito',
-        usuario
-    });
+        res.status(201).json({
+            msg: 'Usuario Creado Con Exito',
+            usuario
+        });
+    }
 }
 
 const usuariosDelete = (req = request, res = response) => {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,12 +4,15 @@ const { usuariosGet,
         usuariosDelete, 
         usuariosPatch, 
         usuariosPut } = require('../controllers/user.controller');
+const { check } = require('express-validator');
 
 const router = Router();
 
 router.get('/', usuariosGet);
 router.put('/:id', usuariosPut);
-router.post('/', usuariosPost);
+router.post('/', [
+        check('correo', 'El correo no es válido').isEmail()
+],usuariosPost);
 router.delete('/', usuariosDelete);
 router.patch('/', usuariosPatch);
 
