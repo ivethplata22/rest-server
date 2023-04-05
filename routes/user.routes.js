5cfcf624c58958694227603756d55fefa579691c
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-04 21:46:26 -0500

    Validar Rol Contra Base de Datos

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const Role = require('../models/role');
 const { usuariosGet, 
         usuariosPost, 
         usuariosDelete, 
         usuariosPatch, 
         usuariosPut } = require('../controllers/user.controller');
-const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -15,7 +16,12 @@ router.post('/', [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password debe ser mayor a 6 letras').isLength({min: 6}),
         check('correo', 'El correo no es válido').isEmail(),
-        check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+        // check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+        check('rol').custom( async(rol = '') => {
+                const existeRol = await Role.findOne({ rol });
+                if(!existeRol)
+                        throw new Error(`El rol ${rol} no esta registrado en la BD`);
+        }),
         validarCampos
 ],usuariosPost);
 router.delete('/', usuariosDelete);
