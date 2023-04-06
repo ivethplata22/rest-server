6669fc70c4cbdbda236912706bfb89aa7551c4c7
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-06 06:20:42 -0500

    Validaciones

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,44 @@
 const { request, response } = require("express");
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const login = (req = request, res = response) => {
-    res.json({
-        msg: 'Login ok'
-    });
+const login = async (req = request, res = response) => {
+    const { correo, password } = req.body;
+
+    try {
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - email'
+            });
+        }
+
+        // Si el usuario esta activo en DB
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if(!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        // Generar el JWT
+
+        return res.status(200).json({
+            msg: 'Login ok'
+        });   
+    } catch (error) {
+        return res.status(500).json({
+            error
+        });
+    }
 }
 
 module.exports = {
