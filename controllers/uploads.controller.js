048e872c08b116426db8b947b3779d7d204ec700
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-08 20:09:01 -0600

    Borrar  imagenes Cloudinary

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -81,10 +81,11 @@ const actualizarImagenCloudinary = async (req = request, res = response) => {
 
     // Limpiar imagenes previas
     if( modelo.img ) {
-        // Hay que borrar la imagen del servidor
-        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
-        if( fs.existsSync(pathImagen) )
-            fs.unlinkSync(pathImagen);
+        // Hay que borrar la imagen del servidor cloudinary
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[ nombreArr.length - 1 ];
+        const [ public_id ] = nombre.split('.');
+        cloudinary.uploader.destroy( public_id );
     }
 
     const { tempFilePath } = req.files.archivo;
